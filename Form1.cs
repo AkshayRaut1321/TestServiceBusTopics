0a22fd996e78907447be1c3c31407f6a551c4c37
akshayraut-gep <akshay.raut@gep.com>
2023-07-01 20:41:33 +0530

    single subscription testing

--- Form1.cs
+++ Form1.cs
@@ -4,22 +4,17 @@
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading;
     using System.Threading.Tasks;
     using System.Windows.Forms;
+    using TestServiceBusTopics.TestingScenarios;
 
     public partial class Form1 : Form
     {
-        string serviceBusConnectionString = "Endpoint=sb://akshayraut.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DGRx184lUgSrKym8p+z66jV+qJ2FMd7l0+ASbMrVlLs=";
+        string serviceBusConnectionString = "Endpoint=sb://akshayraut.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ge+2CAE0f+xd+clzKonx05m5xZdsAj/Mo+ASbM6KG6A=";
         string topicName = "testsessiontopic";
-        string subscriptionName = "TestSubscription";
-        public List<dynamic> LogsList0 { get; set; }
-        public List<dynamic> LogsList1 { get; set; }
-        public List<dynamic> LogsList2 { get; set; }
 
-        private readonly SynchronizationContext synchronizationContext;
         string timeStampFormat = "HH:mm:ss.fff";
+        SingleSubscription singleSubscription;
 
         Dictionary<string, int> messages;
         ConcurrentDictionary<string, ServiceBusSessionProcessor> serviceBusSessionProcessors = new ConcurrentDictionary<string, ServiceBusSessionProcessor>();
@@ -27,13 +22,7 @@
         public Form1()
         {
             InitializeComponent();
-            LogsList0 = new List<dynamic>();
-            LogsList1 = new List<dynamic>();
-            LogsList2 = new List<dynamic>();
-            synchronizationContext = SynchronizationContext.Current; //context from UI thread
-            //logsDataGridView0.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
-            //logsDataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
-            //logsDataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            singleSubscription = new SingleSubscription(this);
         }
 
         private void send_Click(object sender, EventArgs e)
@@ -53,210 +42,22 @@
                 { "Message 10", 1 },
                 { "Message 11", 2 }
             };
-            Task.Run(SendMessagesSingles);
-        }
 
-        private async Task SendMessagesSingles()
-        {
-            ServiceBusClient serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
-            ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(topicName);
-
-            using (ServiceBusMessageBatch serviceBusMessageBatch = await serviceBusSender.CreateMessageBatchAsync())
+            Task.Run(() =>
             {
-                #region Sending one message at a time
-                try
-                {
-                    for (int i = 0; i < messages.Count; i++)
-                    {
-                        var serviceBusMessage = new ServiceBusMessage(messages.ElementAt(i).Key);
-                        serviceBusMessage.SessionId = $"{messages.ElementAt(i).Value}";
-
-                        await serviceBusSender.SendMessageAsync(serviceBusMessage);
-                    }
-
-                    synchronizationContext.Post(new SendOrPostCallback(o =>
-                    {
-                        MessageBox.Show("Messages have been sent.");
-                    }), null);
-                }
-                finally
-                {
-                    await serviceBusSender.DisposeAsync();
-                    await serviceBusClient.DisposeAsync();
-                }
-                #endregion
-            }
-        }
-
-        private async Task SendMessagesBatch()
-        {
-            ServiceBusClient serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
-            ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(topicName);
-
-            using (ServiceBusMessageBatch serviceBusMessageBatch = await serviceBusSender.CreateMessageBatchAsync())
-            {
-                #region Sending messages in a batch
-                for (int i = 0; i < messages.Count; i++)
-                {
-                    var serviceBusMessage = new ServiceBusMessage(messages.ElementAt(i).Key);
-                    serviceBusMessage.SessionId = $"{messages.ElementAt(i).Value}";
-
-                    if (!serviceBusMessageBatch.TryAddMessage(serviceBusMessage))
-                    {
-                        throw new Exception("Message is too large for the topic");
-                    }
-                }
-                try
-                {
-                    await serviceBusSender.SendMessagesAsync(serviceBusMessageBatch);
-
-                    synchronizationContext.Post(new SendOrPostCallback(o =>
-                    {
-                        MessageBox.Show("Messages have been sent.");
-                    }), null);
-                }
-                finally
-                {
-                    await serviceBusSender.DisposeAsync();
-                    await serviceBusClient.DisposeAsync();
-                }
-                #endregion
-            }
+                singleSubscription.SendMessagesSingles(serviceBusConnectionString, topicName, messages);
+            });
         }
 
         private void receive_Click(object sender, EventArgs e)
         {
-            Task.Run(ReceiveMessages);
-        }
-
-        private async Task ReceiveMessages()
-        {
-            for (int i = 0; i < serviceBusSessionProcessors.Count; i++)
+            Task.Run(() =>
             {
-                if (!serviceBusSessionProcessors.ElementAt(i).Value.IsClosed)
-                    await serviceBusSessionProcessors.ElementAt(i).Value.StopProcessingAsync();
-                if (!serviceBusSessionProcessors.ElementAt(i).Value.IsProcessing)
-                    await serviceBusSessionProcessors.ElementAt(i).Value.CloseAsync();
-            }
-
-            ServiceBusClient serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
-
-            var parallelOptions = new ParallelOptions();
-            parallelOptions.MaxDegreeOfParallelism = 3;
-
-            Parallel.For(0, 3, parallelOptions, async (i) =>
-            {
-                // create the options to use for configuring the processor
-                var serviceBusSessionProcessorOptions = new ServiceBusSessionProcessorOptions()
-                {
-                    // By default after the message handler returns, the processor will complete the message
-                    // If I want more fine-grained control over settlement, I can set this to false.
-                    AutoCompleteMessages = true,
-
-                    // I can also allow for processing multiple sessions
-                    MaxConcurrentSessions = 1,
-
-                    // By default or when AutoCompleteMessages is set to true, the processor will complete the message after executing the message handler
-                    // Set AutoCompleteMessages to false to [settle messages](https://docs.microsoft.com/en-us/azure/service-bus-messaging/message-transfers-locks-settlement#peeklock) on your own.
-                    // In both cases, if the message handler throws an exception without settling the message, the processor will abandon the message.
-                    MaxConcurrentCallsPerSession = 1,
-
-                    // Processing can be optionally limited to a subset of session Ids.
-                    SessionIds = { i.ToString() }
-                };
-
-                var serviceBusSessionProcessor = serviceBusClient.CreateSessionProcessor(topicName, subscriptionName, serviceBusSessionProcessorOptions);
-
-                try
-                {
-                    if (i == 0)
-                        serviceBusSessionProcessor.ProcessMessageAsync += ServiceBusProcessor0_ProcessMessageAsync;
-                    else if (i == 1)
-                        serviceBusSessionProcessor.ProcessMessageAsync += ServiceBusProcessor1_ProcessMessageAsync;
-                    else
-                        serviceBusSessionProcessor.ProcessMessageAsync += ServiceBusProcessor2_ProcessMessageAsync;
-
-                    serviceBusSessionProcessor.ProcessErrorAsync += ServiceBusProcessor_ProcessErrorAsync;
-
-                    await serviceBusSessionProcessor.StartProcessingAsync();
-                    serviceBusSessionProcessors.TryAdd(serviceBusSessionProcessor.Identifier, serviceBusSessionProcessor);
-
-                    await Task.Delay(600000);
-                    await serviceBusSessionProcessor.StopProcessingAsync();
-
-                    if (!serviceBusSessionProcessor.IsProcessing)
-                    {
-                        await serviceBusSessionProcessor.CloseAsync();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw;
-                }
-                finally
-                {
-                    await serviceBusSessionProcessor.DisposeAsync();
-                }
-                await serviceBusClient.DisposeAsync();
+                singleSubscription.ReceiveMessages(serviceBusConnectionString, topicName);
             });
         }
 
-        private async Task ServiceBusProcessor_ProcessErrorAsync(ProcessErrorEventArgs arg)
-        {
-            await Task.Run(async () =>
-            {
-                serviceBusSessionProcessors.TryGetValue(arg.Identifier, out ServiceBusSessionProcessor serviceBusSessionProcessor);
-
-                synchronizationContext.Post(new SendOrPostCallback(o =>
-                {
-                    MessageBox.Show(arg.Exception.Message);
-                }), null);
-                if (serviceBusSessionProcessor != null)
-                {
-                    await serviceBusSessionProcessor.StopProcessingAsync();
-                }
-            });
-        }
-
-        private async Task ServiceBusProcessor0_ProcessMessageAsync(ProcessSessionMessageEventArgs arg)
-        {
-            await Task.Run(() =>
-            {
-                //Send the update to our UI thread
-                synchronizationContext.Post(new SendOrPostCallback(o =>
-                {
-                    DisplayLogs(LogsList0, arg, logsDataGridView0);
-                }), null);
-            });
-        }
-
-        private async Task ServiceBusProcessor1_ProcessMessageAsync(ProcessSessionMessageEventArgs arg)
-        {
-            await Task.Run(() =>
-            {
-                //Send the update to our UI thread
-                synchronizationContext.Post(new SendOrPostCallback(o =>
-                {
-                    //DisplayLogs(LogsList1, arg, logsDataGridView1);
-                    DisplayLogs(LogsList0, arg, logsDataGridView0);
-                }), null);
-            });
-        }
-
-        private async Task ServiceBusProcessor2_ProcessMessageAsync(ProcessSessionMessageEventArgs arg)
-        {
-            await Task.Run(() =>
-            {
-                //Send the update to our UI thread
-                synchronizationContext.Post(new SendOrPostCallback(o =>
-                {
-                    //DisplayLogs(LogsList2, arg, logsDataGridView2);
-                    DisplayLogs(LogsList0, arg, logsDataGridView0);
-                }), null);
-            });
-        }
-
-        private void DisplayLogs(List<dynamic> LogsList, ProcessSessionMessageEventArgs arg, DataGridView logsDGV)
+        public void DisplayLogs(List<dynamic> LogsList, ProcessSessionMessageEventArgs arg)
         {
             BindingSource bs = new BindingSource();
             bs.DataSource = LogsList;
@@ -268,29 +69,17 @@
                 Entry = arg.Message.EnqueuedTime.LocalDateTime.ToString(timeStampFormat),
                 Exit = DateTime.Now.ToString(timeStampFormat)
             });
-            logsDGV.DataSource = bs;
-            logsDGV.Refresh();
+            logsDataGridView0.DataSource = bs;
+            logsDataGridView0.Refresh();
         }
 
-        private void clearLogs_Click(object sender, EventArgs e)
+        public void clearLogs_Click(object sender, EventArgs e)
         {
+            singleSubscription.LogsList0 = new List<dynamic>();
             BindingSource bs0 = new BindingSource();
-            bs0.DataSource = LogsList0;
-            LogsList0 = new List<dynamic>();
+            bs0.DataSource = singleSubscription.LogsList0;
             logsDataGridView0.DataSource = bs0;
             logsDataGridView0.Refresh();
-
-            BindingSource bs1 = new BindingSource();
-            bs1.DataSource = LogsList1;
-            LogsList1 = new List<dynamic>();
-            logsDataGridView1.DataSource = bs1;
-            logsDataGridView1.Refresh();
-
-            BindingSource bs2 = new BindingSource();
-            bs2.DataSource = LogsList2;
-            LogsList2 = new List<dynamic>();
-            logsDataGridView2.DataSource = bs2;
-            logsDataGridView2.Refresh();
         }
     }
 }
---
+++ TestingScenarios/SingleSubscription.cs
@@ -0,0 +1,205 @@
+﻿namespace TestServiceBusTopics.TestingScenarios
+{
+    using Azure.Messaging.ServiceBus;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    internal class SingleSubscription
+    {
+        string subscriptionName = "TestSubscription";
+
+        private readonly SynchronizationContext synchronizationContext = SynchronizationContext.Current; //context from UI thread
+
+        Form1 _form;
+        public List<dynamic> LogsList0 { get; set; }
+
+        public SingleSubscription(Form1 form)
+        {
+            _form = form;
+            LogsList0 = new List<dynamic>();
+        }
+
+        public async Task SendMessagesSingles(string serviceBusConnectionString, string topicName, Dictionary<string, int> messages)
+        {
+            ServiceBusClient serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+            ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(topicName);
+
+            using (ServiceBusMessageBatch serviceBusMessageBatch = await serviceBusSender.CreateMessageBatchAsync())
+            {
+                #region Sending one message at a time
+                try
+                {
+                    for (int i = 0; i < messages.Count; i++)
+                    {
+                        var serviceBusMessage = new ServiceBusMessage(messages.ElementAt(i).Key);
+                        serviceBusMessage.SessionId = $"{messages.ElementAt(i).Value}";
+
+                        await serviceBusSender.SendMessageAsync(serviceBusMessage);
+                    }
+
+                    synchronizationContext.Post(new SendOrPostCallback(o =>
+                    {
+                        MessageBox.Show("Messages have been sent.");
+                    }), null);
+                }
+                finally
+                {
+                    await serviceBusSender.DisposeAsync();
+                    await serviceBusClient.DisposeAsync();
+                }
+                #endregion
+            }
+        }
+
+        private async Task SendMessagesBatch(string serviceBusConnectionString, string topicName, Dictionary<string, int> messages)
+        {
+            ServiceBusClient serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+            ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(topicName);
+
+            using (ServiceBusMessageBatch serviceBusMessageBatch = await serviceBusSender.CreateMessageBatchAsync())
+            {
+                #region Sending messages in a batch
+                for (int i = 0; i < messages.Count; i++)
+                {
+                    var serviceBusMessage = new ServiceBusMessage(messages.ElementAt(i).Key);
+                    serviceBusMessage.SessionId = $"{messages.ElementAt(i).Value}";
+
+                    if (!serviceBusMessageBatch.TryAddMessage(serviceBusMessage))
+                    {
+                        throw new Exception("Message is too large for the topic");
+                    }
+                }
+                try
+                {
+                    await serviceBusSender.SendMessagesAsync(serviceBusMessageBatch);
+
+                    synchronizationContext.Post(new SendOrPostCallback(o =>
+                    {
+                        MessageBox.Show("Messages have been sent.");
+                    }), null);
+                }
+                finally
+                {
+                    await serviceBusSender.DisposeAsync();
+                    await serviceBusClient.DisposeAsync();
+                }
+                #endregion
+            }
+        }
+
+        public void ReceiveMessages(string serviceBusConnectionString, string topicName)
+        {
+            ServiceBusClient serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+
+            var parallelOptions = new ParallelOptions();
+            parallelOptions.MaxDegreeOfParallelism = 3;
+
+            Parallel.For(0, 3, parallelOptions, async (i) =>
+            {
+                // create the options to use for configuring the processor
+                var serviceBusSessionProcessorOptions = new ServiceBusSessionProcessorOptions()
+                {
+                    // By default after the message handler returns, the processor will complete the message
+                    // If I want more fine-grained control over settlement, I can set this to false.
+                    AutoCompleteMessages = true,
+
+                    // I can also allow for processing multiple sessions
+                    MaxConcurrentSessions = 1,
+
+                    // By default or when AutoCompleteMessages is set to true, the processor will complete the message after executing the message handler
+                    // Set AutoCompleteMessages to false to [settle messages](https://docs.microsoft.com/en-us/azure/service-bus-messaging/message-transfers-locks-settlement#peeklock) on your own.
+                    // In both cases, if the message handler throws an exception without settling the message, the processor will abandon the message.
+                    MaxConcurrentCallsPerSession = 1,
+
+                    // Processing can be optionally limited to a subset of session Ids.
+                    SessionIds = { i.ToString() }
+                };
+
+                var serviceBusSessionProcessor = serviceBusClient.CreateSessionProcessor(topicName, subscriptionName, serviceBusSessionProcessorOptions);
+
+                try
+                {
+                    if (i == 0)
+                        serviceBusSessionProcessor.ProcessMessageAsync += ServiceBusProcessor0_ProcessMessageAsync;
+                    else if (i == 1)
+                        serviceBusSessionProcessor.ProcessMessageAsync += ServiceBusProcessor1_ProcessMessageAsync;
+                    else
+                        serviceBusSessionProcessor.ProcessMessageAsync += ServiceBusProcessor2_ProcessMessageAsync;
+
+                    serviceBusSessionProcessor.ProcessErrorAsync += ServiceBusProcessor_ProcessErrorAsync;
+
+                    await serviceBusSessionProcessor.StartProcessingAsync();
+
+                    await Task.Delay(600000);
+                    await serviceBusSessionProcessor.StopProcessingAsync();
+
+                    if (!serviceBusSessionProcessor.IsProcessing)
+                    {
+                        await serviceBusSessionProcessor.CloseAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await serviceBusSessionProcessor.DisposeAsync();
+                }
+                await serviceBusClient.DisposeAsync();
+            });
+        }
+        private async Task ServiceBusProcessor_ProcessErrorAsync(ProcessErrorEventArgs arg)
+        {
+            await Task.Run(() =>
+            {
+                synchronizationContext.Post(new SendOrPostCallback(o =>
+                {
+                    MessageBox.Show(arg.Exception.Message);
+                }), null);
+            });
+        }
+
+        private async Task ServiceBusProcessor0_ProcessMessageAsync(ProcessSessionMessageEventArgs arg)
+        {
+            await Task.Run(() =>
+            {
+                //Send the update to our UI thread
+                synchronizationContext.Post(new SendOrPostCallback(o =>
+                {
+                    _form.DisplayLogs(LogsList0, arg);
+                }), null);
+            });
+        }
+
+        private async Task ServiceBusProcessor1_ProcessMessageAsync(ProcessSessionMessageEventArgs arg)
+        {
+            await Task.Run(() =>
+            {
+                //Send the update to our UI thread
+                synchronizationContext.Post(new SendOrPostCallback(o =>
+                {
+                    //DisplayLogs(LogsList1, arg, logsDataGridView1);
+                    _form.DisplayLogs(LogsList0, arg);
+                }), null);
+            });
+        }
+
+        private async Task ServiceBusProcessor2_ProcessMessageAsync(ProcessSessionMessageEventArgs arg)
+        {
+            await Task.Run(() =>
+            {
+                //Send the update to our UI thread
+                synchronizationContext.Post(new SendOrPostCallback(o =>
+                {
+                    //DisplayLogs(LogsList2, arg, logsDataGridView2);
+                    _form.DisplayLogs(LogsList0, arg);
+                }), null);
+            });
+        }
+    }
+}
